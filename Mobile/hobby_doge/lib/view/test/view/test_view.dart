bbc6d8baee2cdc8264f7787ec769dd09553bd587
erturkG <erturkgurses@hotmail.com>
2021-04-01 01:01:24 +0300

    Created test view

---
+++ Mobile/hobby_doge/lib/view/test/view/test_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:hobby_doge/core/base/state/base_state.dart';
+import 'package:hobby_doge/core/base/view/base_view.dart';
+import 'package:hobby_doge/view/test/viewmodel/test_viewmodel.dart';
+
+class TestEkrani extends StatefulWidget {
+  TestEkrani({Key key}) : super(key: key);
+
+  @override
+  _TestEkraniState createState() => _TestEkraniState();
+}
+
+class _TestEkraniState extends BaseState<TestEkrani> {
+  @override
+  Widget build(BuildContext context) {
+    return BaseView(
+      viewModel: TestViewModel(),
+      onModelReady: (model) {
+        model.setContext(context);
+        model.init();
+      },
+      onPageBuilder: (context, value) => Container(
+        height: dynamicHeight(0.1),
+        child: Center(
+          child: Text("Deneme"),
+        ),
+      ),
+    );
+  }
+}
