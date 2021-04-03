763a873f810c233f7a9f5a9e1ab366ed48f716e3
Emre <kosenemre@gmail.com>
2021-04-03 22:49:49 +0300

    Add backend starter project

---
+++ hobbydoge-backend/src/main/java/org/koroglu/hobbydoge/HobbyDogeApplication.java
@@ -0,0 +1,13 @@
+package org.koroglu.hobbydoge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyDogeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyDogeApplication.class, args);
+	}
+
+}
---
+++ hobbydoge-backend/src/test/java/org/koroglu/hobbydoge/HobbyDogeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koroglu.hobbydoge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyDogeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
