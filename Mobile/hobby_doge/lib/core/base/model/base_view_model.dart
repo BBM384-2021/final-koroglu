5d1828b8f026abbd499898d3c36c50576508d3a2
erturkG <erturkgurses@hotmail.com>
2021-04-06 12:52:27 +0300

    Fixed import path

--- Mobile/hobby_doge/lib/core/base/model/base_view_model.dart
+++ Mobile/hobby_doge/lib/core/base/model/base_view_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hobby_doge/core/init/navigation/navgiation_service.dart';
+import 'package:hobby_doge/core/init/navigation/navigation_service.dart';
 
 abstract class BaseViewModel {
   BuildContext context;
