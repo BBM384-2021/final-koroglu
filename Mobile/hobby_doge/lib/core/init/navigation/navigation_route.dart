8ae2dfaf3949f77ec49506b845d5d27df663c93d
erturkG <erturkgurses@hotmail.com>
2021-04-07 16:16:45 +0300

    Created welcome page for unsigned users.

--- Mobile/hobby_doge/lib/core/init/navigation/navigation_route.dart
+++ Mobile/hobby_doge/lib/core/init/navigation/navigation_route.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../view/authantication/login_screen/view/login_view.dart';
 import '../../../view/splash_screen/view/splash_view.dart';
 import '../../../view/test/view/test_view.dart';
+import '../../../view/welcome_sceen/view/welcome_view.dart';
 import '../../constants/navigation_constants.dart';
 
 class NavigationRoute {
@@ -16,6 +18,10 @@ class NavigationRoute {
         return MaterialPageRoute(builder: (context) => TestEkrani());
       case NavigationConstants.SPLASH_VIEW:
         return MaterialPageRoute(builder: (context) => SplashView());
+      case NavigationConstants.LOGIN_VIEW:
+        return MaterialPageRoute(builder: (context) => LoginView());
+      case NavigationConstants.WELCOME_VIEW:
+        return MaterialPageRoute(builder: (context) => WelcomeView());
       default:
         return MaterialPageRoute(
             builder: (context) => Scaffold(
@@ -23,5 +29,4 @@ class NavigationRoute {
                 ));
     }
   }
-
 }
---
+++ Mobile/hobby_doge/lib/view/welcome_sceen/viewmodel/welcome_view_model.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../../../core/base/model/base_view_model.dart';
+import 'package:mobx/mobx.dart';
+part 'welcome_view_model.g.dart';
+
+class WelcomeViewModel = _WelcomeViewModelBase with _$WelcomeViewModel;
+
+abstract class _WelcomeViewModelBase with Store, BaseViewModel {
+  @override
+  void init() {}
+
+  @override
+  void setContext(BuildContext context) => this.context = context;
+}
