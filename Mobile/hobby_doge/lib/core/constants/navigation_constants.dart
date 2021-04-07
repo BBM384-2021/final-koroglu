225cedb4548820fb44b9612e072a4d9e791ce1c8
erturkG <erturkgurses@hotmail.com>
2021-04-07 16:14:43 +0300

    Added login, welcome, signup routes

--- Mobile/hobby_doge/lib/core/constants/navigation_constants.dart
+++ Mobile/hobby_doge/lib/core/constants/navigation_constants.dart
@@ -1,4 +1,7 @@
 class NavigationConstants {
   static const TEST_VIEW = "/test";
   static const SPLASH_VIEW = "/splash";
+  static const LOGIN_VIEW = "/login";
+  static const WELCOME_VIEW = '/welcome';
+  static const SIGNUP_VIEW = "/signup";
 }
