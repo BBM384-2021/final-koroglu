0d9ef4f690d7892c82827cd7ed2ff277aa4bf3a8
erturkG <erturkgurses@hotmail.com>
2021-04-07 16:15:56 +0300

    Added welcome screen texts

--- Mobile/hobby_doge/lib/core/init/lang/locale_keys.g.dart
+++ Mobile/hobby_doge/lib/core/init/lang/locale_keys.g.dart
@@ -4,5 +4,10 @@ abstract class  LocaleKeys {
   static const welcome = 'welcome';
   static const pleaseCheckInternet = 'pleaseCheckInternet';
   static const tryAgain = 'tryAgain';
+  static const welcomeScreen_signup = 'welcomeScreen.signup';
+  static const welcomeScreen_login = 'welcomeScreen.login';
+  static const welcomeScreen_welcomeToHobbyDoge = 'welcomeScreen.welcomeToHobbyDoge';
+  static const welcomeScreen_timeForHobbyDoge = 'welcomeScreen.timeForHobbyDoge';
+  static const welcomeScreen = 'welcomeScreen';
 
 }
