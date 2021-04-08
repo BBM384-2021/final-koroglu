d0a0024a5f8e12fb51a9a1a7aa912e74bef88c99
erturkG <erturkgurses@hotmail.com>
2021-04-09 02:40:24 +0300

    Added error border

--- Mobile/hobby_doge/lib/core/components/common_text_field.dart
+++ Mobile/hobby_doge/lib/core/components/common_text_field.dart
@@ -4,10 +4,7 @@ import '../../core/extensions/context_extension.dart';
 
 class CommonTextField extends StatelessWidget {
   const CommonTextField(
-      {Key key,
-      @required this.text,
-      @required this.textController,
-      this.validator})
+      {Key key, @required this.text, this.textController, this.validator})
       : super(key: key);
   final text;
   final textController;
@@ -15,13 +12,15 @@ class CommonTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: context.width * 0.1),
+      padding: EdgeInsets.symmetric(
+          horizontal: context.width * 0.1, vertical: context.height * 0.001),
       child: TextFormField(
         validator: validator,
         cursorColor: AppColorScheme.instance.grey,
         style: TextStyle(
             color: AppColorScheme.instance.grey, fontWeight: FontWeight.w500),
         decoration: InputDecoration(
+          helperText: ' ', //Given to prevent overflow when error text is showed
           fillColor: AppColorScheme.instance.veryLightGrey,
           filled: true,
           labelText: text,
@@ -39,10 +38,12 @@ class CommonTextField extends StatelessWidget {
               borderSide: BorderSide.none,
               borderRadius: BorderRadius.circular(8)),
           errorBorder: OutlineInputBorder(
-              borderSide: BorderSide.none,
+              borderSide:
+                  BorderSide(color: AppColorScheme.instance.greenLight2),
               borderRadius: BorderRadius.circular(8)),
           focusedErrorBorder: OutlineInputBorder(
-              borderSide: BorderSide.none,
+              borderSide:
+                  BorderSide(color: AppColorScheme.instance.greenLight2),
               borderRadius: BorderRadius.circular(8)),
           disabledBorder: OutlineInputBorder(
               borderSide: BorderSide.none,
