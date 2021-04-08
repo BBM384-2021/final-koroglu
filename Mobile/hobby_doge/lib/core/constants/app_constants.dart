2b1558f6f963b26881b074940ceb2a3e76d021f4
erturkG <erturkgurses@hotmail.com>
2021-04-08 03:49:50 +0300

    Added email regex to app constants

--- Mobile/hobby_doge/lib/core/constants/app_constants.dart
+++ Mobile/hobby_doge/lib/core/constants/app_constants.dart
@@ -1,4 +1,6 @@
 class ApplicationConstants {
   static const LANG_ASSET_PATH = "assets/lang";
   static const FONT_FAMILY = "Lexend";
+  static const EMAIL_REGIEX =
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
 }
