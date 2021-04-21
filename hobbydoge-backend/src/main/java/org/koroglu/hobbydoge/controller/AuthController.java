c00fdc53d81cb2c0ecf174d66843c698881c4054
OzarUn <b21727815@cs.hacettepe.edu.tr>
2021-04-22 00:58:57 +0300

    Move login register to AuthController.

---
+++ hobbydoge-backend/src/main/java/org/koroglu/hobbydoge/controller/AuthController.java
@@ -0,0 +1,32 @@
+package org.koroglu.hobbydoge.controller;
+
+import org.koroglu.hobbydoge.controller.request.LoginRequest;
+import org.koroglu.hobbydoge.controller.request.RegisterRequest;
+import org.koroglu.hobbydoge.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthController {
+
+  @Autowired
+  private UserService userService;
+
+  @PostMapping("/register")
+  public ResponseEntity<?> register(@RequestBody @Valid RegisterRequest registerRequest) throws ParseException {
+    return ResponseEntity.ok().body(userService.register(registerRequest));
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<?> login(@RequestBody @Valid LoginRequest loginRequest) {
+    return ResponseEntity.ok().body(userService.login(loginRequest));
+  }
+}
