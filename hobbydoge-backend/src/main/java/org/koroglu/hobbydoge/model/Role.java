8c22918a9d2864668fe2c70e5d2351348c43e673
ugurcanbagriyanik <ugurcanbagriyanik@odakgis.com.tr>
2021-04-22 10:25:53 +0300

    code clean + register bugfix + auto create roles

--- hobbydoge-backend/src/main/java/org/koroglu/hobbydoge/model/Role.java
+++ hobbydoge-backend/src/main/java/org/koroglu/hobbydoge/model/Role.java
@@ -1,5 +1,6 @@
 package org.koroglu.hobbydoge.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.Set;
 @Setter
 @Entity(name = "roles")
 @NoArgsConstructor
+@AllArgsConstructor
 public class Role {
 
   @Id
