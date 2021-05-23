2f8eda9ff6cc9157817c84df28c8812bafacb90d
Emre <kosenemre@gmail.com>
2021-05-23 20:36:14 +0300

    Change members in ClubDTO

--- hobbydoge-backend/src/main/java/org/koroglu/hobbydoge/dto/mapper/ClubMapper.java
+++ hobbydoge-backend/src/main/java/org/koroglu/hobbydoge/dto/mapper/ClubMapper.java
@@ -3,6 +3,8 @@ package org.koroglu.hobbydoge.dto.mapper;
 import org.koroglu.hobbydoge.dto.model.ClubDTO;
 import org.koroglu.hobbydoge.model.Club;
 
+import java.util.stream.Collectors;
+
 public class ClubMapper {
   public static ClubDTO toClubDTO(Club club) {
     return new ClubDTO()
@@ -11,7 +13,9 @@ public class ClubMapper {
             .setDescription(club.getDescription())
             .setPicture(club.getPicture())
             .setRating(club.getRating())
-            .setMembers(club.getMembers())
+            .setMembers(club.getMembers()
+                    .stream().map(ClubUserMapper::toClubUserDTO)
+                    .collect(Collectors.toSet()))
             .setSubClubs(club.getSubClubs());
 
   }
