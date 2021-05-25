549f7013904a3189d9bc82f486b2c909e13aef93
Emre <kosenemre@gmail.com>
2021-05-25 17:26:24 +0300

    Add update user

--- hobbydoge-backend/src/main/java/org/koroglu/hobbydoge/service/UserService.java
+++ hobbydoge-backend/src/main/java/org/koroglu/hobbydoge/service/UserService.java
@@ -3,6 +3,7 @@ package org.koroglu.hobbydoge.service;
 import org.koroglu.hobbydoge.controller.request.LoginRequest;
 import org.koroglu.hobbydoge.controller.request.RegisterRequest;
 import org.koroglu.hobbydoge.controller.request.ResetPasswordRequest;
+import org.koroglu.hobbydoge.controller.request.UpdateUserRequest;
 import org.koroglu.hobbydoge.dto.model.ListUsersDTO;
 import org.koroglu.hobbydoge.dto.model.LoginDTO;
 import org.koroglu.hobbydoge.dto.model.UserDTO;
@@ -16,6 +17,8 @@ public interface UserService {
   UserDTO getUser(Long id);
 
   List<ListUsersDTO> getUsers(int offset, int limit);
+
+  UserDTO updateUser(Long userId, UpdateUserRequest updateUserRequest);
 
   User getByEmail(String email);
 
