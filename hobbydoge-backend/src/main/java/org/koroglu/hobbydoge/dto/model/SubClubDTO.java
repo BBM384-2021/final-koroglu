94bfdf1b88709ed836a5e1ef58dfc897276198c7
Emre <kosenemre@gmail.com>
2021-05-25 17:29:41 +0300

    Add admin request in subclub

--- hobbydoge-backend/src/main/java/org/koroglu/hobbydoge/dto/model/SubClubDTO.java
+++ hobbydoge-backend/src/main/java/org/koroglu/hobbydoge/dto/model/SubClubDTO.java
@@ -5,9 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
-import org.koroglu.hobbydoge.model.SubClub;
-import org.koroglu.hobbydoge.model.User;
 
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -21,6 +20,8 @@ public class SubClubDTO {
   private String description;
   private String picture;
   private Double rating;
-  private Set<User> members;
-  private Set<SubClub> subClubs;
+  private Set<ClubUserDTO> members;
+  private List<EventDTO> events;
+  private List<ReviewDTO> reviews;
+  private Set<ClubUserDTO> adminRequests;
 }
